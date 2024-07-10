5e6cd97030b016ceed83775f116fd69344e141c8
moh3ndehghani <moh3n.dehghani78@gmail.com>
2024-07-10 18:12:32 +0330

    working on create table

--- src/services/index.js
+++ src/services/index.js
@@ -8,7 +8,7 @@ class IndexDb {
 
     if (!version) {
       const currentVersion = localStorage.getItem(`${this.dbName}-version`);
-      version = currentVersion ? JSON.parse(currentVersion) : 1;
+      version = currentVersion ? JSON.parse(currentVersion) + 1 : 1;
     }
 
     localStorage.setItem(`${dbName}-version`, version);
@@ -20,7 +20,6 @@ class IndexDb {
       this.openReq = indexedDB.open(this.dbName);
 
       this.openReq.onsuccess = function (event) {
-        console.log("hjhhh:", event.target.result);
         this.database = event.target.result;
         resolve(this.database);
       }.bind(this);
@@ -29,24 +28,34 @@ class IndexDb {
         reject(event);
       }.bind(this);
 
-      this.openReq.onupgradeneeded = function (event) {
-        this.database = event.target.result;
-        // this.database.createObjectStore("users", { keyPath: "id" });
-      }.bind(this);
+      // this.openReq.onupgradeneeded = function (event) {}.bind(this);
     });
   }
 
   createTable(tableName, config) {
+    return new Promise((resolve, reject) => {
+      const transaction = this.database.transaction(tableName, "readwrite");
+    });
+
+    // const objectStore = transaction.objectStore(tableName);
+
+    // transaction.oncomplete = () => {
+    //   resolve(`Table '${tableName}' created successfully.`, objectStore);
+    // };
+
+    // transaction.onerror = (event) => {
+    //   reject(`Error creating table '${tableName}': ${event.target.error}`);
+    // };
     // this.openReq.onupgradeneeded = function (event) {
     //   console.log("event ===", event);
-    this.database.createObjectStore(tableName, config);
+    //   this.database.createObjectStore(tableName, config);
     // }.bind(this);
   }
 
-  #increaseVersion() {
-    this.version = this.version + 1;
-    localStorage.setItem(`${this.dbName}-version`, this.version);
-  }
+  // #increaseVersion() {
+  //   this.version = this.version + 1;
+  //   localStorage.setItem(`${this.dbName}-version`, this.version);
+  // }
 }
 
 export default IndexDb;
